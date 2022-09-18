a6d9d5eaacadb7837584ced179f5e8fb0b1d6aad
am22ar <amarpatil12222@gmail.com>
2022-09-18 11:23:34 +0530

    Added: Addition Logic

--- src/com/java/calculator/Calculator.java
+++ src/com/java/calculator/Calculator.java
@@ -4,7 +4,10 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Calculator Program");
-
+		int num1 = 10;
+		int num2 = 20;
+		int add = num1 + num2;
+		System.out.println("Addition is: "+add);
 	}
 
 }
