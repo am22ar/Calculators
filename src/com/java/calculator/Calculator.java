0dedeb05ed39bc3cd932f3940d0f0d7469827844
am22ar <amarpatil12222@gmail.com>
2022-09-18 11:28:01 +0530

    Added: Substraction Logic

--- src/com/java/calculator/Calculator.java
+++ src/com/java/calculator/Calculator.java
@@ -4,10 +4,12 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Calculator Program");
-		int num1 = 10;
-		int num2 = 20;
+		int num1 = 20;
+		int num2 = 12;
 		int add = num1 + num2;
+		int sub = num1 - num2;
 		System.out.println("Addition is: "+add);
+		System.out.println("Substraction is: "+sub);
 	}
 
 }
